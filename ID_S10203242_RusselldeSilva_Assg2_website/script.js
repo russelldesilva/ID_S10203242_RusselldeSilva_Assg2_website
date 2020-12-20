64431128074ca9b532ca9606abee36c8fcd35020
RusselldeSilva <s10203242@connect.np.edu.sg>
2020-12-20 12:49:51 +0800

    basic html/css

--- ID_S10203242_RusselldeSilva_Assg2_website/script.js
+++ ID_S10203242_RusselldeSilva_Assg2_website/script.js
@@ -5,3 +5,17 @@ function newPage(url){
 
 $("#clock").click(function(){newPage("bus-timings.html")});
 $("#route").click(function(){newPage("fastest-route.html")});
+
+$("#see-fastest").hover(
+    function(){
+        $(this).css({"background-color":"#2699fb",})
+        $("a").css({"color":"white"})
+        $("path").css({"stroke":"white"})
+    }, 
+    function(){
+        $(this).css({"background-color":"white",})
+        $("a").css({"color":"#2699fb"})
+        $("path").css({"stroke":"#2699fb"})
+    }
+);
+$("#see-fastest").click(function(){newPage("fastest-route.html")});
